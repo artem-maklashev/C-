ea36e9c00527060cf6f0b0d3e6913a574e987911
Маклашев Артем <artem@maklashev.ru>
2022-09-29 11:15:32 +0400

    HomrWork 2frends added

---
+++ 2frends/Program.cs
@@ -0,0 +1,21 @@
+﻿int count = 0;
+double distance = 10000; 
+int firstFrendSpeed = 1, secondFrendSpeed = 2, dogSpeed = 5, friend = 2;
+double time_ = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time_ = distance / (firstFrendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time_ = distance / (secondFrendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFrendSpeed + secondFrendSpeed) * time_;
+    count = count + 1;
+}
+Console.Write("Собака пробежит " + count +" раз!");
