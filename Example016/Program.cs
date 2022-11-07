3acb583aee3796ce84a2f4f1a48269305c606cea
Artem Maklashev <artem@maklashev.ru>
2022-11-07 15:38:51 +0400

    Урок 6

---
+++ Example016/Program.cs
@@ -0,0 +1,13 @@
+﻿//Factorial
+
+double Factorial(int n)
+{
+    if (n==0) return 1; 
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
---
+++ Example017/Program.cs
@@ -0,0 +1,16 @@
+﻿// Числа Фибоначчи
+// f(1) =1;
+// f(2) = 1;
+//f(n)=f(n-1)+f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == -1) return 1;
+    if (n == 0) return 0;
+    else return (Fibonacci(n+2) - Fibonacci(n +1));
+}
+
+for (int i = 0; i > -11; i=i-1)
+{
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
